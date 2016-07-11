ced37971db773e732c3f234b545cf46249ddfa37
YUHAO-ZX <helloworldzx@gmaill.com>
2016-07-11 19:45:26 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,11 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Stopwatch;
+import com.google.common.util.concurrent.RateLimiter;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import test.T;
 
 import java.math.BigDecimal;
@@ -8,13 +13,27 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by xinz on 2016/6/12.
  */
 public class Test {
+    static Logger logger = LoggerFactory.getLogger(Test.class);
     public static void main(String[] args) throws ParseException, InterruptedException, ExecutionException {
-                
-    }
+//        logger.error("testfff");
 
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        RateLimiter r = RateLimiter.create(10);
+        for(int i=0;i<10;i++){
+            System.out.println(r.tryAcquire(5));
+            if(i==0){
+                Thread.sleep(700);
+            }
+        }
+        stopwatch.stop();
+        System.out.println(stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    }
 }
--- src/main/java/security/Tester.java
+++ src/main/java/security/Tester.java
@@ -6,14 +6,28 @@ package security;
 public class Tester {
     final static String key = "8e2bf219";
     public static void main(String[] args) {
-        String password = "123456";
-        try {
-            String rs = AESUtils.encrypt(password,key);
-            System.out.println(rs);
-            rs = AESUtils.decrypt(rs,key);
-            System.out.println(rs);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        String password = "123456";
+//        try {
+//            String rs = AESUtils.encrypt(password,key);
+//            System.out.println(rs);
+//            rs = AESUtils.decrypt(rs,key);
+//            System.out.println(rs);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        System.out.println(getSign());
+    }
+
+    private static String getSign(){
+
+        StringBuilder buf = new StringBuilder();
+        buf.append("carts=[{\"buyer_id\":\"1\",\"extra\":\"\",\"weibo_iid\":\"1\"}]");
+        buf.append("9a58577aae3b2e878876b160e4a49b8e");
+//        buf.append("buyer_id=" + "1344513317");
+//        buf.append("&buyer_source=" + "sina_weibo");
+//        buf.append("&items=" + "[{\"sku_no\":\"df2768694050176987\",\"unique_product_id\":\"deal_df160627p2768694\",\"quantity\":\"1\",\"seller_type\":\"sina_weibo\",\"seller_id\":\"1305672987\",\"seller_product_id\":\"110013056729870003033509\",\"seller_extra\":\"\",\"sellType\":\"WeiboLive\",\"sellLabel\":123,\"add_extra\":\"\",\"add_time\":1467342794}]");
+//        buf.append("27243547mefdg92hh35626p1b7s9fz7y");
+        System.out.println(buf.toString());
+        return MD5Util.encrypt(buf.toString());
     }
 }
