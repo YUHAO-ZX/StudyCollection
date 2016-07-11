1d5043eb0834f23b7d6d9cbf77dbcec5a560e667
YUHAO-ZX <helloworldzx@gmaill.com>
2016-07-11 20:59:31 +0800

    test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,39 +1,20 @@
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.base.Stopwatch;
-import com.google.common.util.concurrent.RateLimiter;
-import org.apache.commons.lang.StringUtils;
+import limiter.MyRateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import test.T;
 
-import java.math.BigDecimal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
 import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Created by xinz on 2016/6/12.
  */
 public class Test {
     static Logger logger = LoggerFactory.getLogger(Test.class);
     public static void main(String[] args) throws ParseException, InterruptedException, ExecutionException {
-//        logger.error("testfff");
-
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        RateLimiter r = RateLimiter.create(10);
-        for(int i=0;i<10;i++){
-            System.out.println(r.tryAcquire(5));
-            if(i==0){
-                Thread.sleep(700);
-            }
+//        RateLimiter r = RateLimiter.create(10);
+        MyRateLimiter r = new MyRateLimiter(7);
+        for(int i=0;i<20;i++){
+            System.out.println(r.tryAcquire());
+            Thread.sleep(100);
         }
-        stopwatch.stop();
-        System.out.println(stopwatch.elapsed(TimeUnit.MILLISECONDS));
     }
 }
