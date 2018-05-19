7e3cb7dbace0f04b086b4b14db3a4d20593608d5
yuhao.zx <yuhao.zx@antfin.com>
2018-05-19 21:34:55 +0800

    test2

--- src/main/java/git/CherryPickTest.java
+++ src/main/java/git/CherryPickTest.java
@@ -10,4 +10,7 @@ package git;
  * @version $Id: CherryPickTest.java, v 0.1 2018年05月19日 下午9:31 yuhao.zx Exp $
  */
 public class CherryPickTest {
+    public static void main(String[] args) {
+        System.out.println("test 1111");
+    }
 }
