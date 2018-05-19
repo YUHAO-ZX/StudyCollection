e9e74bd121203684397dfc90f4d19c51b689724f
yuhao.zx <yuhao.zx@antfin.com>
2018-05-19 21:34:55 +0800

    test2
    # Conflicts:
    #	src/main/java/git/CherryPickTest.java

--- src/main/java/git/CherryPickTest.java
+++ src/main/java/git/CherryPickTest.java
@@ -11,6 +11,6 @@ package git;
  */
 public class CherryPickTest {
     public static void main(String[] args) {
-        System.out.println("test 2222");
+        System.out.println("test 1111");
     }
 }
